3147514ac248afa385f1516af6ee065b225a2ec4
generous-vector-9581 <ojappah@gmail.com>
2019-09-26 00:29:59 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -22,7 +22,7 @@ def retrieval
                   "oil paintings" => 3
                  }
 
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
+shipping_manifest["oil paintings"]  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 
 end
 
@@ -33,13 +33,13 @@ def adding
                   "oil paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
-
+shipping_manifest["muskets" = 2]
 
 
   # add 4 gun powder to the shipping_manifest hash below
-
+shipping_manifest["gun powder" = 4]
 
 
   # return the shipping_manifest hash below
-
+return shipping_manifest
 end
